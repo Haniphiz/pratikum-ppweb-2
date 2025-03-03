ca15268f7aea90ef64c692aec58b3809f00f2d20
Haniphiz <150140674+Haniphiz@users.noreply.github.com>
2025-03-03 08:48:29 +0700

    Add files via upload

---
+++ pertemuan-2/variabel_user.php
@@ -0,0 +1,12 @@
+<?php
+    //mendefinisikan variabel   
+    $nama = "Hanip";
+    $umur = "18";
+    $bb = "56";
+
+
+    echo " Nama saya adalah : " .$nama;
+    echo "<br/> Umur : " .$umur. " Tahun";
+    echo "<br/> Berat badan : " .$bb. " Kg";
+
+?>
